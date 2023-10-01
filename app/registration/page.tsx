ccc3e5896c84044ffccaf6aa12b89b39a1baa310
unknown <sprout20091008@gmail.com>
2023-10-01 16:25:43 +0900

    feat/registration 新規作成

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import "./page.scss";
+import Image from "next/image";
+import { useState } from 'react';
+
 const Registration: React.FC = () => {
-  return <p>新規登録ページ</p>;
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
+  return (
+    <div className="registration">
+      <h2>新規会員登録</h2>
+      <div className="container">
+        <label htmlFor="email" className="label">メールアドレス</label>
+        <input type="email" name="email" id="email" className="input" autoFocus />
+        <label htmlFor="password" className="label">パスワード</label>
+        <div>
+          <input type={passwordShown ? "text" : "password"} name="password" id="password" className="input" />
+          <span onClick={togglePassword} role="presentation" className="eye">{passwordShown ? <Image src={require("@/assets/svg/eye.svg")} alt="eye" /> : <Image src={require("@/assets/svg/eye-slash.svg")} alt="/eye-slash" />}</span>
+        </div>
+      </div>
+      <div className="register">
+        <button type="submit" className="reg-button">
+          <Image src={require("@/assets/svg/envelope.svg")} alt="envelope" /> 同意して新規登録する(無料)
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
